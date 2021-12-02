6174ab01d344b594a5f39830c10dfd548d8a0132
Kirsty <kirstysays@gmail.com>
2021-12-02 18:10:35 +0000

    Day Two, both parts (v. hacky brute force code)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,102 @@
-use std::{fs, io::Lines};
+use std::{fs, io::Lines, error};
 
 fn main() {
     println!("AoC Day 1");
 
     println!("Reading in the file...");
 
-    let str_contents = fs::read_to_string("dayone_actual.txt").expect("Something went wrong reading the file.");
+    let str_contents = fs::read_to_string("daytwo_actual.txt").expect("Something went wrong reading the file.");
   
     //depth_counter_one_step(str_contents);
 
-    depth_counter_three_step(str_contents);
+    //depth_counter_three_step(str_contents);
+    let use_aim : bool = true;
+    submarine_course_calculator(str_contents, use_aim);
 }
+
+fn submarine_course_calculator(input : String, use_aim : bool)
+{
+    let split_string : Vec<&str> = input.split_whitespace().collect();
+
+    let mut idx : i32 = 1;
+    
+    let mut horizontal : i32 = 0;
+    let mut vertical : i32 = 0;
+   
+    let mut horizontalMuliplyer : i32 = 0;
+    let mut verticalMultiplyer : i32 = 0;
+    let mut aim = 0;
+
+    for section in split_string
+    {     
+        // We've got a number to go by
+        if idx % 2 == 0
+        {
+            let section_num : i32 = section.parse().expect("Unable to parse to i32");
+
+            
+            horizontal += section_num * horizontalMuliplyer;
+
+             if (!use_aim)
+             {
+                vertical += section_num * verticalMultiplyer;
+                
+             }
+             else 
+             {
+                aim += section_num * verticalMultiplyer;    
+                
+                // We need to move as well
+                if (horizontalMuliplyer != 0)
+                {
+                    println!("aim is = {}, horizontal is {}", aim, horizontal);
+                    vertical += section_num * aim;
+                    println!("vertical {}", vertical);
+                }
+
+             }
+            
+
+            horizontalMuliplyer = 0;
+            verticalMultiplyer = 0;
+        }
+        else
+        {
+            if section.eq("forward")
+            {
+                horizontalMuliplyer = 1;
+            }
+            // back isn't used but keeping here in case
+            else if (section.eq("back"))
+            {
+                horizontalMuliplyer = -1;
+            }
+            else if (section.eq("up"))
+            {
+                verticalMultiplyer = -1;
+            }
+            else if (section.eq("down"))
+            {
+                verticalMultiplyer = 1;
+            }
+            else
+            {
+                println!("unable to interpret section {}",  section);
+            }
+
+        }
+        
+        idx+=1;
+        
+    }
+
+    let answer = horizontal * vertical;
+
+    println!("horizontal {}, vertical {} : answer {}", horizontal, vertical, answer);
+
+    //assert!(answer == 1636725);
+}
+
 
 fn depth_counter_one_step(input : String)
 {
